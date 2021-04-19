9b1c67c8f41cdab3ad7757357705cae7949158d7
Marton Lederer <marton@lederer.hu>
2021-04-19 16:23:39 +0200

    fix: make __line an actual variable

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ export const filelocPlugin = (options?: FilelocPluginOptions): Plugin => ({
           relativefilename: "${path.relative(rootDir, args.path)}",
           relativedirname: "${path.relative(rootDir, path.dirname(args.path))}"
         };
+        let __line = 0;
       `;
         const fileContent = new TextDecoder().decode(
           await fs.readFile(args.path)
@@ -29,13 +30,15 @@ export const filelocPlugin = (options?: FilelocPluginOptions): Plugin => ({
         const lines = fileContent.split("\n");
         let fileWithCharsAndLines = "";
 
-        for (let i = 0; i < lines.length; i++)
+        for (let i = 0; i < lines.length; i++) {
+          const hasLineNumber = !!lines[i].match(/__line/g);
           fileWithCharsAndLines +=
-            lines[i].replace(/__line/g, (i + 1).toString()) + "\n";
+            (hasLineNumber ? `__line=${i + 1};` : "") + lines[i] + "\n";
+        }
 
         const globalsRegex = /__(?=(filename|dirname|relativefilename|relativedirname))/g;
         const contents =
-          variables +
+          (fileWithCharsAndLines.match(globalsRegex) ? variables : "") +
           "\n" +
           fileWithCharsAndLines.replace(globalsRegex, "__fileloc.");
         const loader = args.path.split(".").pop() as Loader;
